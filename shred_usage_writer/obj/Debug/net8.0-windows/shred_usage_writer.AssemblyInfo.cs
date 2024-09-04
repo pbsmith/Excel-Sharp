824e9e7a8f46dff8c1bb825266c7b2e6e5c49511
Pendle <124411365+pbsmith@users.noreply.github.com>
2024-09-04 19:51:13 -0400

    Establish block type A backend part 1

--- shred_usage_writer/obj/Debug/net8.0-windows/shred_usage_writer.AssemblyInfo.cs
+++ shred_usage_writer/obj/Debug/net8.0-windows/shred_usage_writer.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("shred_usage_writer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fe8a0fd1324905164d9c727ff8d7bf54af6e8227")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+578919dd43a6a6632339bbf3229592aee77df8e1")]
 [assembly: System.Reflection.AssemblyProductAttribute("shred_usage_writer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("shred_usage_writer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
